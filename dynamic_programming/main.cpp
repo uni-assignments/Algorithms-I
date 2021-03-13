eb6dad5843be27ab91e9bfc26a546eeb437086ad
ritarez <ritarezborges@gmail.com>
2021-03-12 23:43:56 -0300

    fixed for  when theres a discount on first line

--- dynamic_programming/main.cpp
+++ dynamic_programming/main.cpp
@@ -10,17 +10,24 @@ float minimum_value(int line, int d, vector<vector<pair<float, float>>> memo){
     int min = 0;
     for(int i = 0; i <= line && i < d; i++){
         if(memo[line][i].second < memo[line][min].second) min = i;
-
     }
     return memo[line][min].second;
 }
-
+void print_memo(int d, int n, vector<vector<pair<float, float>>> memo){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i or j < d; j++){
+            cout << memo[i][j].second <<" ";
+        }
+        cout << endl;
+    }
+}
 float minimum_cost(int n, int d, int t, vector<float> discounts, vector<pair<float, float>> subway_lines){
     vector<vector<pair<float, float>>> memo(n, vector<pair<float, float>> (d));
     
     memo[0][0] = subway_lines[0];
+    memo[0][0] = make_pair(subway_lines[0].first, subway_lines[0].second * discounts[0]);
     for(int i = 1; i < n; i++){
-        memo[i][0] = make_pair(subway_lines[i].first, subway_lines[i].second + minimum_value(i-1, d, memo)); //primeira coluna o tempo 0, logo o tempo sera so o dql escala e o custo sera o menor custo
+        memo[i][0] = make_pair(subway_lines[i].first, subway_lines[i].second * discounts[0] + minimum_value(i-1, d, memo)); //primeira coluna o tempo 0, logo o tempo sera so o dql escala e o custo sera o menor custo
         for(int j = 1; j < d and j <= i; j++){ 
             if(memo[i-1][j-1].first < t and memo[i-1][j-1].first >= 0)
                 memo[i][j] = make_pair(subway_lines[i].first + memo[i-1][j-1].first, subway_lines[i].second * discounts[j] + memo[i-1][j-1].second);
@@ -28,15 +35,18 @@ float minimum_cost(int n, int d, int t, vector<float> discounts, vector<pair<flo
                 memo[i][j] = make_pair(-1, INF);
         }
     }
+    //print_memo(d, n, memo);
     return minimum_value(n - 1, d, memo);
 }
 
 
 vector<float> transform_discounts(vector<float> discounts, int d_sz){
-    float d = discounts[0];
     for(int i = 1; i < d_sz; i++){
-        d += discounts[i];
-        discounts[i] = (100 - d)/100;
+        discounts[i] += discounts[i-1];
+        if(discounts[i] > 100.0) discounts[i] = 100.0;
+    }
+    for(int i = 0; i < d_sz; i++){
+        discounts[i] = (100.0 - discounts[i])/100.0;
     }
     return discounts;
 }
